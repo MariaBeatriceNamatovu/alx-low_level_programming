88728c5f46753076a33f822fa5e8c211b32a7d59
MariaBeatriceNamatovu <namatovutrismari@gmail.com>
2023-05-03 11:51:52 -0700

    pointers

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,7 +2,8 @@
 
 /**
  * reset_to_98 - function that takes a pointer  to an int and updates it
- * @n - the value to be updated to 98
+ * @n: the value to be updated to 98
+ * dsescription: function that takes a pointer to an int and updates it
  * return: always 0
  */
 
