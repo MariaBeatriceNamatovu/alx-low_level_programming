620d7a5b7b29d49df3108107d31bed0f5abc2e66
MariaBeatriceNamatovu <namatovutrismari@gmail.com>
2023-05-02 20:20:45 -0700

    strings

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -10,15 +10,15 @@ void rev_string(char *s)
 	char temp;
 
 	for (length = 0; s[length] != '\0'; length++)
-	;
-	z = 0
+	
+	z = 0;
 		half = length / 2;
 
 	while (half--)
 	{
 		temp = s[length - z - 1];
 		s[length - z - 1] = s[z];
-		s[z]  temp;
+		s[z] = temp;
 		z++;
 	}
 }
