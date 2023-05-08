aa3d734e00789218387eebd8ff772fc91db99b00
MariaBeatriceNamatovu <namatovutrismari@gmail.com>
2023-05-08 13:13:16 -0700

    pointers

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -2,9 +2,6 @@
  /**
   * print_chessboard - entry point prints chessboard
   * @a: array
-  *
-  * Description: this function  prints the  chessboard
-  *print_chessboard: (prints the chess board)
   * Return: Always 0 (success)
   */
 
@@ -15,7 +12,7 @@ void print_chessboard(char (*a)[8])
 	for (i = 0; i < 8; i++)
 	{
 		for (n = 0; n < 8; n++)
-			putchar(a[i][n]);
-		putchar('\n');
+			_putchar(a[i][n]);
+		_putchar('\n');
 	}
 }
