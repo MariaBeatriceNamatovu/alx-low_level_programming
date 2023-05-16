f648c493bbd15f0f336656e85526f0a9b9447bcc
MariaBeatriceNamatovu <namatovutrismari@gmail.com>
2023-05-16 12:17:25 -0700

    greatness comes from service

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * free_grid - frees a two dimensional grid
+ * @grid: the two dimesnsional grid
+ * @height: height of grid
+ *
+ * Return: nothing
+ */
+
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+	{
+		free(grid[i]);
+	}
+	free(grid);
+}
